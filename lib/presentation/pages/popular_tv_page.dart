ad2fbdcec647a46d1d828999ebb07e23d1b93241
deffaalaika <deffa41@gmail.com>
2022-05-23 11:04:50 +0700

    commit

---
+++ lib/presentation/pages/popular_tv_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class PopularTvPage extends StatefulWidget {
+
+  static const ROUTE_NAME = '/popular-tv';
+
+ const PopularTvPage({Key? key}) : super(key: key);
+
+  @override
+  _PopularTvPageState createState() {
+    return _PopularTvPageState();
+  }
+}
+
+class _PopularTvPageState extends State<PopularTvPage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold();
+  }
+}
---
+++ lib/presentation/pages/top_rated_tv_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class TopRatedTvPage extends StatefulWidget {
+  static const ROUTE_NAME = '/top-rated-tv';
+const  TopRatedTvPage({Key? key}) : super(key: key);
+
+  @override
+  _TopRatedTvPageState createState() {
+    return _TopRatedTvPageState();
+  }
+}
+
+class _TopRatedTvPageState extends State<TopRatedTvPage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold();
+  }
+}
